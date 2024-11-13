b68ea8c0eeba8dbfb1407ad4fbf278536bea1d90
Jans Molina <jvns.molina1@gmail.com>
2024-11-13 15:32:14 -0300

    Creacion del ejercicio 2 sobre Cadenas

---
+++ 02_Cadenas/script2.py
@@ -0,0 +1,19 @@
+# Forma de combinar str con int usando F String
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+print(" ")
+
+# Muestra el precio con 2 decimales
+price = 59
+txt1 = f"The price is {price:.2f} dollars"
+print(txt1)
+
+# Se puede agregar una operacion
+txt2 = f"The price is {20 * 59} dollars"
+print(txt2)
+
+# Ejemplo en un print
+edad = 23
+print (f"Me llamo Jans y tengo {23} años de edad")
